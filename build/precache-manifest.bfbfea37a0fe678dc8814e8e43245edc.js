0b985bcf937d9bd76d2a17f40f41a0d3a0975f5e
Jamil Naeem <jamil.naeem17@gmail.com>
2020-07-01 21:16:03 -0400

    Built app in preparation to deploy using Heroku

---
+++ build/precache-manifest.bfbfea37a0fe678dc8814e8e43245edc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7d68b627adf7dd585ce12e802d4f75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55bbf5504870ac6f3afe",
+    "url": "/static/js/2.de214602.chunk.js"
+  },
+  {
+    "revision": "a942a563e26d1aff35ef",
+    "url": "/static/js/main.a0786e58.chunk.js"
+  },
+  {
+    "revision": "1a13a14f40bc4acbc696",
+    "url": "/static/js/runtime-main.b00d9aa4.js"
+  }
+]);
